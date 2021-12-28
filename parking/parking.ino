323ee3db40f399a95b39f9cac5f49671f9b1fac0
Maryam <maryam.eloraby@gmail.com>
2021-12-29 00:50:41 +0200

    Update parking.ino

--- parking/parking.ino
+++ parking/parking.ino
@@ -1,8 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <Arduino_FreeRTOS.h>
+
+//ultrasonic 1
 const int trigger_pin = A2;
 const int echo_pin = A3;
+int duration;
+int distance;
+
+//ultrasonic 2
 const int trigger_pin2 = A5;
 const int echo_pin2 = A4;
+int duration2;
+int distance2;
+
+int tn = 4000;
+
+//buzzer pin
 const int buzzer = 6;
 
 // Motor A
@@ -17,21 +31,18 @@ int in4 = 8;
 
 int wheelspeed = 100;
 
-
-int tn = 4000;
-int duration;
-int distance;
-
-int duration2;
-int distance2;
-
 bool moveforward = false;
 bool movebackward = false;
 bool stopCar = false;
 bool ultrasonic1range = false;
 bool ultrasonic2range = false;
 
+//buttons to choose which way to park
+const int button1 = 9;
+const int button2 = 10;
 
+bool buttonclicked1 = false;
+bool buttonclicked2 = false;
 
 void setup() {
 
@@ -47,6 +58,37 @@ void setup() {
   pinMode(echo_pin, INPUT);
   pinMode(trigger_pin2, OUTPUT);
   pinMode(echo_pin2, INPUT);
+
+
+  pinMode(button1, INPUT); digitalWrite(button1, HIGH);
+  pinMode(button2, INPUT); digitalWrite(button2, HIGH);
+
+
+
+  xTaskCreate(
+    taskOne,          /* Task function. */
+    "Parking",        /* String with name of task. */
+    128,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
+
+  xTaskCreate(
+    taskTwo,          /* Task function. */
+    "Ultrasonics",        /* String with name of task. */
+    128,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
+
+
+  xTaskCreate(
+    taskThree,          /* Task function. */
+    "Ultrasonics2",        /* String with name of task. */
+    128,            /* Stack size in bytes. */
+    NULL,             /* Parameter passed as input of the task */
+    1,                /* Priority of the task. */
+    NULL);            /* Task handle. */
 
   Serial.begin(9600);
 }
@@ -94,7 +136,7 @@ void turnAForwardandBBackward()
 void turnoffMotors() {
   analogWrite(enA, 0);
   analogWrite(enB, 0);
-  
+
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
@@ -149,11 +191,10 @@ void ultrasonic1() {
   Serial.print("u1 ");
   Serial.println(distance);
 
-
-
   if (distance <= 7 && distance > 0)
   {
     tone(buzzer, tn);
+    if (buttonclicked2 == true) stopNow();
   }
 
   else if (distance <= 13)
@@ -184,7 +225,7 @@ void ultrasonic2() {
   digitalWrite(trigger_pin2, LOW);
   duration2 = pulseIn(echo_pin2, HIGH);
   distance2 = (duration2 / 2) / 28.5;
-  //  noTone(buzzer);
+  //noTone(buzzer);
   Serial.print("u2 ");
   Serial.println(distance2);
 
@@ -192,6 +233,7 @@ void ultrasonic2() {
   if (distance2 <= 7 && distance2 > 0)
   {
     tone(buzzer, tn);
+    if (buttonclicked1 == true) stopNow();
   }
 
   else if (distance2 <= 13)
@@ -229,9 +271,8 @@ void parkCar() {
   /* the car turns right first , and then keeps moving backwards until it gets close to a wall,
     then turns left until and moves forward until it gets close to a wall. */
   if (step == 1) {
-    //if (count < 10) {
     turnBForwardandABackward();
-    delay(240);
+    delay(390);
     stopNow();
     step = 2;
     delay(1000);
@@ -239,29 +280,35 @@ void parkCar() {
   }
   if (step == 2) {
     BothBack();
-    delay(575);
-
-    // if (distance2 <= 60 && distance2 > 0) {
+    delay(530);
     stopNow();
     delay(1000);
     step = 3;
-    // }
   }
-  //}
 
   if (step == 3) {
     turnAForwardandBBackward();
-    delay(200);
+    delay(380);
     stopNow();
     step = 4;
   }
   if (step == 4) {
     BothForward();
-    delay(200);
+    delay(130);
     stopNow();
 
     step = 5;
   }
+
+  if (step == 5)
+  {
+
+    turnBForwardandABackward();
+    delay(220);
+    stopNow();
+    step = 6;
+  }
+
 }
 
 
@@ -269,9 +316,8 @@ void parkCarOpposite() {
   /* the car turns right first , and then keeps moving backwards until it gets close to a wall,
     then turns left until and moves forward until it gets close to a wall. */
   if (step == 1) {
-    //if (count < 10) {
     turnAForwardandBBackward();
-    delay(240);
+    delay(300);
     stopNow();
     step = 2;
     delay(1000);
@@ -279,7 +325,7 @@ void parkCarOpposite() {
   }
   if (step == 2) {
     BothForward();
-    delay(575);
+    delay(560);
     stopNow();
     delay(1000);
     step = 3;
@@ -287,27 +333,75 @@ void parkCarOpposite() {
 
   if (step == 3) {
     turnBForwardandABackward();
-    delay(200);
+    delay(395);
     stopNow();
     step = 4;
   }
   if (step == 4) {
     BothBack();
-    delay(200);
+    delay(130);
     stopNow();
+
     step = 5;
   }
-  
+
+  if (step == 5)
+  {
+
+    turnAForwardandBBackward();
+    delay(220);
+    stopNow();
+    step = 6;
+  }
+
+
 }
 
 
 
+void taskOne() {
+  while (1) {
+    if (digitalRead(button2) == LOW) {
+      Serial.println(" 2 pressed");
+      buttonclicked2 = true;
+      parkCar();
+      step = 1;
+    }
+
+
+    if (digitalRead(button1) == LOW) {
+      Serial.println(" 1 pressed");
+      buttonclicked1 = true;
+      parkCarOpposite();
+      step = 1;
+    }
+
+  }
+
+}
+
+void taskTwo() {
+
+
+  while (1) {
+    ultrasonic1();
+    delay(800);
+  }
+
+}
+
+
+void taskThree() {
+
+
+  while (1) {
+    ultrasonic2();
+    delay(800);
+  }
+
+}
+
 
 void loop() {
-  //ultrasonic1();
-  //ultrasonic2();
-  //parkCar();
-  //parkCarOpposite();
-
 
 }
